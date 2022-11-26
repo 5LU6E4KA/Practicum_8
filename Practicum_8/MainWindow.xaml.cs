72756bdbb10f316812439428f3488d9e535932fe
Dmitry <dimavitalev6@gmail.com>
2022-11-26 17:37:24 +0300

    new marks

--- Practicum_8/MainWindow.xaml.cs
+++ Practicum_8/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ComponentModel;
 
 namespace Practicum_8
 {
@@ -23,17 +24,43 @@ namespace Practicum_8
         public MainWindow()
         {
             InitializeComponent();
+            Point.IsChecked = true;
+            Closing += WindowClosing;
         }
 
         private void StartCheckClick(object sender, RoutedEventArgs e)
         {
             Box.SpellCheck.IsEnabled = true;
         }
+
         private void StartClearClick(object sender, RoutedEventArgs e)
         {
-            Box.SpellCheck.IsEnabled = false;
-            Box.Clear();
+            const string message = "Вы уверены, что желаете очистить запись?";
+            const string caption = "Form cleaning";
+            var result = MessageBox.Show(message, caption, MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if(result == MessageBoxResult.Yes)
+            {
+                Box.SpellCheck.IsEnabled = false;
+                Box.Clear();
+            }
         }
 
+        private Dictionary<string, string> languages = new Dictionary<string, string> { { "Русский", "ru" }, { "Английский", "en" } };
+
+        private void ClickLanguage(object sender, RoutedEventArgs e)
+        {
+            Box.Language = System.Windows.Markup.XmlLanguage.GetLanguage(languages[((RadioButton)sender).Content.ToString()]);
+        }
+
+        private void WindowClosing(object sender, CancelEventArgs e)
+        {
+            const string message = "Вы уверены, что хотите закрыть приложение?";
+            const string caption = "Form closing";
+            var result = MessageBox.Show(message, caption, MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
     }
 }
